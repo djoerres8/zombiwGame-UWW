0590ccfccfd0a60704e320d8381a08ec9edf981e
Jake <Jake@172.22.238.117>
2017-02-19 16:35:42 -0600

    Added tank class

---
+++ src/zombiewar/impl/Tank.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar.impl;
+
+/**
+ *
+ * @author Jake
+ */
+public class Tank extends Character implements IZombie{
+
+	private static int objCount = 0;
+	private int id=0;
+	
+	/**
+	 * A tank starts with 150 points of health
+	 */
+	public Tank() {
+		super(150);
+		id = objCount++;
+	}
+
+	/**
+	 * An attack from the tank decrease a survivor's health by 20 points.
+	 * @param survivor 
+	 */
+	
+	public void attack(ISurvivor survivor) {
+		survivor.decreaseHealth(20);
+	}
+	
+	public String toString() {
+		return "Tank " + id;
+	}
+}
